140f5c0735f0adc15823c84f8080a402939886b6
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-08-30 22:00:15 +0200

    delete fewTasks.js

--- src/Tasks/fewTasks.js
+++
@@ -1,6 +0,0 @@
-export default [
-    { id: 1, content: "Zjeść kolację", done: false },
-    { id: 2, content: "Kupić spodnie", done: true },
-    { id: 3, content: "Odrobić lekcje", done: false },
-    { id: 4, content: "wyjść z psem na spacer" },
-]
