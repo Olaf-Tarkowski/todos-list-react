d51d21001d8433de01bc26961c54810771e06445
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-02 17:45:45 +0200

    Add instant focus on input

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import { StyledForm, NewTasks, StyledButton } from "./styled"
 import { useFocus } from "./useFocus";
 
 const Form = ({ addNewTaskContent }) => {
-    const { inputRef, focusInput } = useFocus();
+    const { inputRef } = useFocus();
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -17,7 +17,7 @@ const Form = ({ addNewTaskContent }) => {
     return (
         <StyledForm onSubmit={onFormSubmit}>
             <NewTasks ref={inputRef} value={newTaskContent} onChange={(event) => setNewTaskContent(event.target.value)} type="text" placeholder="Co jest do zrobienia?" />
-            <StyledButton onClick={focusInput}>
+            <StyledButton>
                 Dodaj zadanie
             </StyledButton>
         </StyledForm>
--- src/Form/useFocus.js
+++ src/Form/useFocus.js
@@ -1,9 +1,9 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 export const useFocus = () => {
     const inputRef = useRef(null);
-    const focusInput = () => {
-        inputRef.current.focus()
-    };
-    return { inputRef, focusInput };
+    useEffect(() => {
+        inputRef.current.focus();
+    });
+    return { inputRef };
 };
