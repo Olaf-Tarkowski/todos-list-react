b8f02c8da32246344fa7361b5e65389af7328751
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-11-19 20:36:24 +0100

    create tasksSaga

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,25 @@
+import { takeEvery, call, put, delay, select} from "redux-saga/effects";
+import { getExampleTasks } from "./getExampleTasks";
+import { setTasksInLocalStorage } from "./tasksInLocalStorage";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+
+function* fetchExampleTasksHendler() {
+    try{
+        yield delay(1_000);
+        const exampleTasls = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasls));
+
+    }catch(error){
+        yield call(alert, "Coś nie poszło zgodnie z oczekiwaniami")
+    }
+}
+
+function* saveTasksInLocalStorageHendler() {
+        const tasks = yield select(selectTasks)
+        yield call(setTasksInLocalStorage, tasks)
+};
+
+export function* tasksSaga() {
+        yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHendler);
+        yield takeEvery("*", saveTasksInLocalStorageHendler);
+    }
