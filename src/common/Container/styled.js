b38264a942dc190fbe5165380efa960a87bb15af
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-11-28 20:10:15 +0100

    made adjustment in max-width and margin

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const StyledMain = styled.main`
-    max-width: 1050px;
+    max-width: 900px;
     margin: 0 auto;
+    @media(max-width: 939px) {
+        margin: 0px 20px;
+    }
 `;
