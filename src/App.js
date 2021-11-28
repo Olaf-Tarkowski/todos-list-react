05168ba5f05610ca69b2998bef3a101f4c98cf6d
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-11-28 19:40:25 +0100

    created navigation in react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import Tasks from "./features/tasks/Tasks"
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage"
+import Author from "./features/author/Author";
+import TaskPage from "./features/tasks/TaskPage";
+import { StyledList, StyledNavLink } from "./styled";
 
-const App = () => <Tasks />;
+const App = () => (
+    <HashRouter>
+        <nav>
+            <StyledList>
+                <li><StyledNavLink to="/zadania">Lista zadań</StyledNavLink></li>
+                <li><StyledNavLink to="/autor">O autorze</StyledNavLink></li>
+            </StyledList>
+        </nav>
+        <Switch>
+            <Route path="/zadania/:id">
+                <TaskPage />
+            </Route>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/autor">
+                <Author />
+            </Route>
+            <Redirect to="/zadania" />
+        </Switch>
+    </HashRouter>
+);
 
 export default App;
